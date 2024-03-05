4083d9a13b7ab55e67d0f6cda6a23bc07d1c5b86
Jonathan Robichaud <jonathanrobichaud@icloud.com>
2024-03-05 14:46:27 -0400

    initial commit!

---
+++ custom_components/nanoleaf/const.py
@@ -0,0 +1,18 @@
+"""Constants for Nanoleaf integration."""
+
+DOMAIN = "nanoleaf"
+
+NANOLEAF_EVENT = f"{DOMAIN}_event"
+
+TOUCH_MODELS = {"NL29", "NL42", "NL52"}
+
+EMERSION_MODELS = {"NL69"}  # Emersion is probably a typo, should be "Immersion?"
+
+EMERSION_MODES = ["1D", "2D", "3D", "4D"]
+
+TOUCH_GESTURE_TRIGGER_MAP = {
+    2: "swipe_up",
+    3: "swipe_down",
+    4: "swipe_left",
+    5: "swipe_right",
+}
